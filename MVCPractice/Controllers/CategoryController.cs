f8f2bae839f7fcfc22326f021303afa6db860e16
ihemo <ihemo@DESKTOP-C0H68RD>
2025-08-01 15:57:44 +0600

    Add project files.

---
+++ MVCPractice/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using MVCPractice.Models;
+
+namespace MVCPractice.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+           _context = context;
+        }
+        public IActionResult Index()
+        {
+          var categoriesList = _context.Categories.ToList();
+            return View(categoriesList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index","Category");
+        }
+
+    }
+}
---
+++ MVCPractice/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace MVCPractice.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Order Id")]
+        public int OrderId { get; set; }
+    }
+}
